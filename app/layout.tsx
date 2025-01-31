f48eb2ef8d8cccbf777ada716e225471502b6eed
Joe Alongi <email@heyitsjoealongi.com>
2025-01-30 22:21:20 -0500

    Update Colors and Meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+      <body className="max-w-xl mt-[9vh] mx-9 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           {children}
           <Footer />
--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,21 @@ export const metadata: Metadata = {
   title: "heyitsjoealongi",
   description:
     "Leading end-to-end application development throughout the agile lifecycle.",
+  openGraph: {
+    title: "heyitsjoealongi",
+    description:
+      "Leading end-to-end application development throughout the agile lifecycle",
+    images: ["/app/heyitsjoealongi.png"],
+  },
 };
 
 export default function Page() {
   return (
     <section>
-      <h1 className="inline-block h-fit w-fit self-center uppercase text-xl min-[2000px]:text-3xl font-slab font-medium subpixel-antialiased">
+      <h1 className="uppercase text-xl min-[2000px]:text-3xl font-slab font-medium subpixel-antialiased mx-auto max-w-xl">
         heyitsjoealongi
       </h1>
-      <p className="my-3 min-[2000px]:my-6 text-xl min-[2000px]:text-3xl font-extralight subpixel-antialiased">
+      <p className="my-3 lg:my-6 text-xl lg:text-3xl font-extralight subpixel-antialiased">
         Leading end-to-end application development throughout the agile
         lifecycle. Coding in JavaScript, TypeScript, Java, C#/.NET, Python, SQL,
         and NoSQL for cloud native applications and on-premises systems.
