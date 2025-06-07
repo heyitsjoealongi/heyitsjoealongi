796bbbfc89d9358f1a67b912245383d122f3b8cf
Joe Alongi <email@heyitsjoealongi.com>
2025-06-07 15:06:54 -0400

    Update Packages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
       <h1 className="max-w-xl mx-auto text-3xl lg:text-6xl font-light subpixel-antialiased">
         heyitsjoealongi
       </h1>
-      <h3 className="my-9 lg:my-18 max-w-xl mx-auto text-xl lg:text-3xl font-light subpixel-antialiased">
+      <h3 className="my-3 lg:my-6 max-w-xl mx-auto text-xl lg:text-3xl font-light subpixel-antialiased">
         Advising on resilience, engineering security, and shaping what’s next.
       </h3>
-      <p className="my-3 lg:my-6 text-md lg:text-lg font-extralight subpixel-antialiased">
+      <p className="text-md lg:text-lg font-extralight subpixel-antialiased">
         Leading end-to-end application development throughout the agile
         lifecycle. Coding in JavaScript, TypeScript, Java, C#/.NET, Python, SQL,
         and NoSQL for cloud native applications and on-premises systems.
